bd793a50242c9a7bce7675ff2fc61bdedd02ee5d
Jacartas <goncalomatospereira@gmail.com>
2020-07-27 11:56:17 +0100

    Added safearea

--- lib/pages/Cases.dart
+++ lib/pages/Cases.dart
@@ -12,258 +12,253 @@ class _CasesState extends State<Cases> {
   String country='two';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(0,0,0,0),
-      child: Column(
-        children: <Widget>[
-          ClipPath(
-            clipper: OvalBottomBorderClipper(),
-            child: Container(
-              height: 200,
-              color: Colors.blue,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.fromLTRB(0,0,0,0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20,),
+            Container(
+              child: DropdownButton <String> (
+                value: country,
+                onChanged: (String newValue){
+                  setState(() {
+                    country = newValue;
+                  });
+                },
+                items: <String>['two','three','four'].map<DropdownMenuItem<String>>((String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList()
+              ),
             ),
-          ),
-          SizedBox(height: 20,),
-          Container(
-            child: DropdownButton <String> (
-              value: country,
-              onChanged: (String newValue){
-                setState(() {
-                  country = newValue;
-                });
-              },
-              items: <String>['two','three','four'].map<DropdownMenuItem<String>>((String value){
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList()
-            ),
-          ),
-          Card(
-            margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                Text(
-                  "Last  24H",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+            Card(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Text(
+                    "Last  24H",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.brightness_1,color: Colors.red,),
-                                Text(
-                                  "Cases",
-                                  style: TextStyle(
-                                    fontSize: 20,
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.brightness_1,color: Colors.red,),
+                                  Text(
+                                    "Cases",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 10,),
-                            Text(
-                              "1000",
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red,
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.brightness_1,color: Colors.black,),
-                                Text(
-                                  "Deaths",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 10,),
-                            Text(
-                              "1000",
-                              style: TextStyle(
+                              SizedBox(height: 10,),
+                              Text(
+                                "1000",
+                                style: TextStyle(
                                   fontSize: 30,
-                                  fontWeight: FontWeight.bold
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.brightness_1,color: Colors.green,),
-                                Text(
-                                  "Recovered",
-                                  style: TextStyle(
-                                    fontSize: 20,
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.brightness_1,color: Colors.black,),
+                                  Text(
+                                    "Deaths",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
                                   ),
+                                ],
+                              ),
+                              SizedBox(height: 10,),
+                              Text(
+                                "1000",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold
                                 ),
-                              ],
-                            ),
-                            SizedBox(height: 10,),
-                            Text(
-                              "1000",
-                              style: TextStyle(
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.brightness_1,color: Colors.green,),
+                                  Text(
+                                    "Recovered",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10,),
+                              Text(
+                                "1000",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Card(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Text(
+                    "Total",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.brightness_1,color: Colors.red,),
+                                  Text(
+                                    "Cases",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10,),
+                              Text(
+                                "1000",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.brightness_1,color: Colors.black,),
+                                  Text(
+                                    "Deaths",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10,),
+                              Text(
+                                "1000",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.brightness_1,color: Colors.green,),
+                                  Text(
+                                    "Recovered",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10,),
+                              Text(
+                                "1000",
+                                style: TextStyle(
                                   fontSize: 30,
                                   fontWeight: FontWeight.bold,
                                   color: Colors.green,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20,),
-              ],
-            ),
-          ),
-          SizedBox(height: 20,),
-          Card(
-            margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                Text(
-                  "Total",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                    ],
                   ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.brightness_1,color: Colors.red,),
-                                Text(
-                                  "Cases",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 10,),
-                            Text(
-                              "1000",
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.brightness_1,color: Colors.black,),
-                                Text(
-                                  "Deaths",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 10,),
-                            Text(
-                              "1000",
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.brightness_1,color: Colors.green,),
-                                Text(
-                                  "Recovered",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 10,),
-                            Text(
-                              "1000",
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20,),
-              ],
+                  SizedBox(height: 20,),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
